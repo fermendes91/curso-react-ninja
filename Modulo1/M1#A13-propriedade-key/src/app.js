f0ded1d0c9d992284e2e32704eb4ccce9f8f845a
Fernando Mendes <fernando.mendes91@outlook.com>
2019-06-25 22:08:50 -0300

    M1#A13-propriedade-key
    Entendimento da propriedade key, evitar a sua duplicidade e como tratar a duplicidade.

--- Modulo1/M1#A13-propriedade-key/src/app.js
+++ Modulo1/M1#A13-propriedade-key/src/app.js
@@ -2,41 +2,47 @@
 
 import React, { Component } from 'react'
 import Title from './title'
+import Square from './square'
 
-// Renderizando componente a partir de class extendendo de um React.Componente.
 class App extends Component{
     render() {
         return (
-            // prop -> propriedade name no componente Title
             <div  data-id='1' className='container'>
-                <Title name='Thaysa' lastname='Maciel' age={25} isMasculino={false} carro={{marca: 'Chevrolet', modelo: 'Celta'}}/>
+                <Title name='Thaysa' lastname='Macieeel' age={25} isMasculino={false} carro={{marca: 'Chevrolet', modelo: 'Celta'}}/>
                 <label htmlFor='input'>Input</label>
+                
+                <br/><br/>
+                <Square color='violet' />
+
+                <br/><br/>
+                <Square />
+                
+                <br/><br/>
+                {['orange', 'gray', 'pink'].map((square) => (
+                    <Square key={square} color={square} />
+                ))}
+                <br/><br/>
+
+                <h2>Square com chaves duplicadas, React junta todos os duplicados em 1 unico componente, por isso os 3 viraram somente 1 square</h2>
+                {['orange', 'gray', 'pink'].map((square) => (
+                    <Square key='key' color={square} />
+                ))}
+
+                <h2>Utilizando o index do array para definir a key faz com que os 3 squares por mais que tenham cores iguais sejam renderizados sem sobreposição.</h2>
+                {['blue', 'red', 'blue'].map((square, index) => (
+                    <Square key={index} color={square} />
+                ))}
+
             </div>
-            // class e for são palavras registradas do javascript, por esse motivo usamos className e htmlFor para identifcarmos estes atributos para html.
         )
     }
 }
+// é necessario identificar "key", para cada componente dentro de um array, os tornando unicos.
 
-// forma de utilizar valores default para variavei que não foram passadas ao componente
+// ao duplicarmos uma key o React vai nos informar que existem 2 componentes com a mesma KEY... e JUNTA os componentes em 1 só.
+
 App.defaultProprs = {
     name: 'Desconhecido'
 }
 
-/*
-    // App no caso será a nossa aplicação, para o hot-loader funcionar corretamente os componentes devem ser centralizados dentro de 1 pagina responsavel por toda a aplicação.
-    // esta pagina no caso é este .js chamado app.js 
-
-    const App = React.createClass({
-    render: function () {
-        return (
-            // prop -> propriedade name no componente Title
-            <div  data-id='1' className='container'>
-                <Title name='Thaysa' lastname='Maciel' age={25} isMasculino={false} carro={{marca: 'Chevrolet', modelo: 'Celta'}}/>
-                <label htmlFor='input'>Input</label>
-            </div>
-            // class e for são palavras registradas do javascript, por esse motivo usamos className e htmlFor para identifcarmos estes atributos para html.
-        )
-    }
-})*/
-
-export default App // esta importando o APP, utilizando codigo do ES2015
+export default App 
