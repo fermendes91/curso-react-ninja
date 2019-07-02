54326323f9e1d9989fbbb432a85df13255c74b22
Fernando Mendes <fernando.mendes91@outlook.com>
2019-07-01 21:29:14 -0300

    M1#A20-LifeCycle
    estudo sobre metodos invocados ao atualizarmos um componente.

--- Modulo1/M1#A20-lifecycle/src/app.js
+++ Modulo1/M1#A20-lifecycle/src/app.js
@@ -6,34 +6,41 @@ import Timer from './timer'
 // componente stateful -> Aquele que manipula o estado.
 class App extends Component {
   constructor () {
-    console.log('CONSTRUCTOR')
+    console.log('constructor APP')
     super()
     this.state = {
+      time: 0,
       showTimer: true
     }
-  }
-
-  // METODO QUE SERÁ EXECUTADO ANTES DO COMPONENTE SER CRIADO.
-  // O componentWillMount() SERA EXECUTADO POSTERIORMENTE AO CONSTRUCTOR. E O ANTERIORMENTE AO RENDER
-  componentWillMount () {
-    console.log('COMPONENT WILL MOUNT')
   }
 
   // METODO QUE SERÁ EXECUTADO POSTERIOR AO COMPONENTE SER CRIADO.
   // O componentDidMount() SERA EXECUTADO POSTERIORMENTE AO CONSTRUCTOR. E O POSTERIORMENTE AO RENDER
   componentDidMount () {
-    console.log('COMPONENT DID MOUNT')
+    console.log('componentDidMount APP')
+  }
+
+  // METODO QUE SERÁ EXECUTADO ANTES DO COMPONENTE SER CRIADO.
+  // O componentWillMount() SERA EXECUTADO POSTERIORMENTE AO CONSTRUCTOR. E O ANTERIORMENTE AO RENDER
+  componentWillMount () {
+    console.log('componentWillMount APP')
   }
 
   render () {
-    console.log('RENDER');
+    console.log('render APP');
     // na linha 35 temos um "curto-circuito" javascript que verificar se a variavel showTimer é true e se o <Timer /> também e true., caso sim o componente é renderizado.
     return (
       <div>
-        {this.state.showTimer && <Timer />}
+        {this.state.showTimer && <Timer time={this.state.time} />}
         <button onClick={() => {
           this.setState({showTimer: !this.state.showTimer})
         }}>Show / hide timer</button>
+        <button onClick={() => {
+          this.setState({time: this.state.time + 5 })
+        }}>Change Props.</button>
+        <button onClick={() => {
+          this.setState({time: 20})
+        }}>Not Render</button>
       </div>
     )
   }
--- Modulo1/M1#A20-lifecycle/src/timer.js
+++ Modulo1/M1#A20-lifecycle/src/timer.js
@@ -3,33 +3,61 @@
 import React, { Component } from 'react'
 
 class Timer extends Component {
-    constructor () {
-        super()
-        this.state = {
-            time: 0
-        }
-
-        this.timer;
+  constructor() {
+    console.log('constructor TIMER')
+    super()
+    this.state = {
+      time: 0
     }
 
-    // depois que o componente for criado, vai executar este metodo para que nosso timer seja incrementado de 1 em 1 segundo
-    componentDidMount() {
-        this.timer = setInterval(() => {
-            this.setState({time: this.state.time + 1})
-        }, 1000)
-    }
+    this.timer;
+  }
 
-    // METODO QUE SERÁ EXECUTADO ANTES DO COMPONENTE SER DESTRUIDO
-    // tirando esse metodo é possivel identificar um problema que ficara exibindo no console, devindo o interval tentar atualizar um valor que nao esta mais em tela.
-    componentWillUnmount () {
-        clearInterval(this.timer)
-    }
+  // depois que o componente for criado, vai executar este metodo para que nosso timer seja incrementado de 1 em 1 segundo
+  componentDidMount () {
+    console.log('componentDidMount TIMER')
+    this.timer = setInterval(() => {
+      this.setState({ time: this.state.time + 1 })
+    }, 1000)
+  }
 
-    render() {
-        return (
-            <div>Timer: {this.state.time}</div>
-        )
-    }
+  // METODO QUE SERÁ EXECUTADO ANTES DO COMPONENTE SER DESTRUIDO
+  // tirando esse metodo é possivel identificar um problema que ficara exibindo no console, devindo o interval tentar atualizar um valor que nao esta mais em tela.
+  componentWillUnmount () {
+    console.log('componentWillReceiveProps TIMER')
+    clearInterval(this.timer)
+  }
+
+  // Quando o componente vai receber uma propriedade antes de receber ele cai neste metodo, o this.props são as propriedades atuais e o nextProps será as propriedades que estão vindo.
+  // na primeira renderizacao do componente ele não é executado, somente quando recebe alguma propriedade nova.
+  // é neste momento que poderemos utilizar o this.setState().
+  componentWillReceiveProps (nextProps) {
+    console.log('componentWillReceiveProps TIMER', this.props, nextProps)
+
+  }
+
+  // metodo que verifica se o nosso componente necessita ser atualizado ou não. Ele é executado antes do render
+  shouldComponentUpdate (nextProps, nextState) {
+    return this.props.time !== nextProps.time
+  }
+
+  // sera executado no momento em que o componente será atualizado. Ultimo ponto antes do componente atualizar.
+  // neste ponto não é aconselhavel usar o this.setState()
+  componentWillUpdate (nextProps, nextState) {
+    console.log('componentWillUpdate TIMER', this.props, nextProps)
+  }
+
+  // sera executado apos a renderizacao do componente. Recebe por parametros recebe as propriedades anteriores e o estado anterior da aplicação.
+  componentDidUpdate (prevProps, prevState) {
+    console.log('componentDidUpdate TIMER', this.props, prevProps)
+  }
+  
+  render() {
+    console.log('render TIMER')
+    return (
+      <div>Timer: {this.state.time}</div>
+    )
+  }
 }
 
 export default Timer
