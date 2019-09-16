5aa6dd122880457da3ed69ed3c0f07c41ff6ae8b
Fernando Mendes <fernando.mendes91@outlook.com>
2019-09-16 14:47:51 -0300

    M2#A30 - Storybook
    Rodando a aplicação Storybook e entendendo seu funcionamento.

--- Modulo2/M2.A30-Storybook/src/app.js
+++ Modulo2/M2.A30-Storybook/src/app.js
@@ -6,7 +6,7 @@ import React from 'react'
 // esta pagina no caso é este .js chamado app.js 
 const App = React.createClass({
     render: function () {
-        return <div>Aplicação</div>
+        return <div>My App</div>
     }
 })
 
