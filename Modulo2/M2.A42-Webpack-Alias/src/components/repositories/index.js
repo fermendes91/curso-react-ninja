1f769f5325e2b80d130ab3a5d90ffcaa6dc4a315
Fernando Mendes <fernando.mendes91@outlook.com>
2019-09-17 23:32:08 -0300

    M2#A42 - Webpack Alias

---
+++ Modulo2/M2.A42-Webpack-Alias/src/components/repositories/index.js
@@ -0,0 +1,31 @@
+'use strict'
+
+import React, {PropTypes} from 'react'
+
+import style from './repositories.css'
+
+const Repositories = ({ className, title, repos }) => (
+ 
+  <div className={style.repos}>
+    <h2>{title}</h2>
+    <ul>
+      {repos.map((repo, index) => (
+        <li key={index}>
+          <a href={repo.link}>{repo.name}</a>
+        </li>
+      ))}
+    </ul>
+  </div >
+)
+
+Repositories.defaultProps = {
+  className: ''
+}
+
+Repositories.propTypes = {
+  className: PropTypes.string,
+  title: PropTypes.string.isRequired,
+  repos: PropTypes.array
+}
+
+export default Repositories
