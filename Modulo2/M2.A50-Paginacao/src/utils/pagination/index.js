fb46dbd4dbf32d56424e881f3b8731c8f4ba45fb
Fernando Mendes <fernando.mendes91@outlook.com>
2019-09-26 00:20:54 -0300

    M2#A54 - Paginacao
    Implementação de testes e paginação

--- Modulo2/M2.A50-Paginacao/src/utils/pagination/index.js
+++ Modulo2/M2.A50-Paginacao/src/utils/pagination/index.js
@@ -1,7 +1,48 @@
 'use strict'
 
-const pagination = ({total, activePage}) => {
-    return Array.apply(null, leng)
+const centerRule = ({total, activePage}) => {
+  if(activePage - 1 <= 0) {
+    return 1;
+  }
+  return activePage - 1;
+}
+
+const pagination = ({ total, activePage }) => {
+  /* até o map a implementação esta criando um array de undefineds
+    este array terá o tamanho da variavel passada como parametro ( total )
+    convertemos este array de undefineds para o que precisamos
+    se recebermos 5, temos que retornar um array com 5 paginas 
+    [1, 2, 3, 4, 5].
+  
+    O _ na chamada representa um item que não será utilizado na iteração,
+    como é um array de undefineds que estamos iterando não é necessario
+    passar valor, então o _ representa este dado que não será usado
+    pela implementação.
+    */
+
+  // ES5 
+  //return Array.apply(null, { length: total }).map((_, i) => i + 1);
+  
+  // ES6 -> o Array.from como segundo parametro já recebe um metodo map()
+
+  if (total <= 5) {
+    return Array.from({length: total}, (_, i) => i + 1);
+  }
+
+  const visiblePages = 3
+  let pages = [
+    1,
+    ...Array.from({length: visiblePages}, (_, i) => i + centerRule({ total, activePage })),
+    total
+  ]
+    
+  // removendo itens duplicados do Array
+  pages = pages.filter((page, index, array) => {
+    return array.indexOf(page) === index;
+  })
+
+  return pages;
+
 }
 
 export default pagination
