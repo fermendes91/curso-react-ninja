b90942ef8e9ec39c0bd417d2199091ded1da8839
Fernando Mendes <fernando.mendes91@outlook.com>
2019-10-15 15:01:34 -0300

    A77 - Promises / AwaitAsync
    Estudo de promises e utilização de await async

---
+++ Modulo2/A77-Promises/src/app.js
@@ -0,0 +1,16 @@
+'use strict'
+
+import React, { Component } from 'react'
+import Title from 'components/Title'
+
+class App extends Component {
+  render () {
+    return (
+      <div>
+        <Title>My App</Title>
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ Modulo2/A77-Promises/src/components/Title/index.js
@@ -0,0 +1,14 @@
+'use strict'
+
+import React, { Component } from 'react'
+
+class Title extends Component {
+
+    contructor(props) {
+        super(props)
+    }
+
+    
+}
+
+export default Title;
